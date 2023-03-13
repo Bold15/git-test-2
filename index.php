66479ad043bcf900dfc5da4a43d284498b2353b6
Alexander Oktaviyanto <alexander151004@gmail.com>
2023-03-14 00:01:16 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur officiis ab esse reprehenderit sapiente saepe ullam, assumenda excepturi fugiat ut voluptatibus magnam, iste adipisci commodi, molestiae at sunt nisi et!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
